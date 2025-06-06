0111b48f8841600f6c6a7d140dbf2cbad81b2838
StarLiink <servicosmt65@gmail.com>
2025-06-05 21:27:17 -0400

    Add files via upload

---
+++ js/2dc5438a46de7fd6.pt-BR.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["pt-BR"] = Object.assign(window['cmsg']['assets']["pt-BR"] || {}, {"15706":{"js":["6018f2c7a77c016c.strings.js"],"css":[]},"38701":{"js":["15ac57b7445b85df.strings.js"],"css":[]}});
